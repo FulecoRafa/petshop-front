cda4c1c43b95f3ff3bbabfb28093bcdfb80d0d0c
Edws <eduardos.rocha17@gmail.com>
2020-07-21 21:50:22 -0300

    Created Shop page

--- src/views/client/Shop.vue
+++ src/views/client/Shop.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div class='shop'>
         <SecondHeader title='Shop'></SecondHeader>
         <SearchBox></SearchBox>
-        <Card>
-            <Product name='Dog food' price='20.99' marca='Crunchie bites' desc='Box food for dogs'>
-                <img src='@/assets/dog-food.jpg' />
+        <Card class='products'>
+            <Product v-for='(i, key) in products' :key='key' :name='i.name' :price='i.price' :marca='i.marca' :desc='i.desc'>
+                <img :src='require(`@/assets/${i.src}`)' :alt='i.alt'>
             </Product>
         </Card>
     </div>
@@ -16,8 +16,25 @@ import Product from '@/components/Product'
 import SearchBox from '@/components/SearchBox'
 import SecondHeader from '@/components/SecondHeader'
 
+const createProduct = (name, price, marca, desc, src, alt) => ({
+    name, price, marca, desc, src, alt
+})
+
 export default {
     name: 'Shop',
+    data: () => ({
+        a: ['n', 'b', 'c'],
+        products: [
+            createProduct('Dog food', 20.99, 'Crunchie bites', 'Box food for dogs', 'dog-food.jpg', 'dog-food'),
+            createProduct('Cat food', 20.99, 'Whiskas', 'Snacks for cats', 'catfood.jpg', 'cat-food'),
+            createProduct('Colar', 5.99, 'Sheffield', 'Collar with guide for walk', 'coleira.jpeg', 'colar'),
+            createProduct('Food Bowl', 2.99, 'Sheffield', 'Food bowl of different colors', 'pratinhodecomida.jpg', 'bowl'),
+            createProduct('Pet bed', 9.99, 'Sheffield', 'Comfortable little bed for your pet to sleep in', 'petbed.jpg', 'pet-bed'),
+            createProduct('Aquarium', 49.99, 'Le Fisherman', 'Large 20L aquarium with filter', 'aquario.jpeg', 'aquarium'),
+            createProduct('Dog Toy', 1.99, 'Sheffield', 'Munching toy shapped like a bone', 'dogtoy.jpg', 'dog-toy'),
+            createProduct('Bird cage', 9.99, 'Birds of paradise', 'Small cage for small birds', 'birdcage.jpg', 'bird-cage')
+        ]
+    }),
     components: {
         Card,
         Product,

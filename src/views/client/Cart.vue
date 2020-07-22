fad04c3e86e213b59068e4eae7196937420132c1
Edws <eduardos.rocha17@gmail.com>
2020-07-22 01:23:22 -0300

    Now the cart iterate in a object to take the data

--- src/views/client/Cart.vue
+++ src/views/client/Cart.vue
@@ -1,21 +1,45 @@
 <template>
-    <div>
-        <CartItem value='0' name='Dog food' marca='Crunch bites' price='20.99'
-            @remove='remove(0)' @increment='increment(0)' @decrement='decrement(0)'
-            @update='x => update(0, x)'>
-            <img src='@/assets/dog-food.jpg' alt='dog-food'>
-        </CartItem>
+    <div class='cart'>
+        <div v-for='({quantity, product} , key) in items' :key='key'>
+            <CartItem :value='quantity' :name='product.name' :marca='product.marca' :price='product.price'
+            @remove='remove(key)' @increment='increment(key)' @decrement='decrement(key)'
+            @update='x => update(key, x)'>
+                <img :src='require(`@/assets/${product.src}`)' :alt='product.alt'>
+            </CartItem>
+            <hr>
+        </div>
     </div>
 </template>
 
 <script>
 import CartItem from '@/components/CartItem'
 
+const createProduct = (name, price, marca, desc, src, alt) => ({
+    name, price, marca, desc, src, alt
+})
+
+
 export default {
     name: 'Cart',
     components: {
         CartItem
     },
+    data: () => ({
+        items: [
+            {
+                quantity: 1,
+                product: createProduct('Dog food', 20.99, 'Crunchie bites', 'Box food for dogs', 'dog-food.jpg', 'dog-food')
+            },
+            {
+                quantity: 2,
+                product: createProduct('Cat food', 20.99, 'Whiskas', 'Snacks for cats', 'catfood.jpg', 'cat-food'), 
+            },
+            {
+                quantity: 1,
+                product: createProduct('Colar', 5.99, 'Sheffield', 'Collar with guide for walk', 'coleira.jpeg', 'colar'),
+            }
+        ]
+    }),
     methods: {
         remove(item) {
             console.log(`Remove ${item}`)

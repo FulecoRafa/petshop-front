29c04c2a3f8f39e5788b759e7458f66b1168a5f2
Edws <eduardos.rocha17@gmail.com>
2020-07-22 02:06:35 -0300

    Improving Cart

--- src/views/client/Cart.vue
+++ src/views/client/Cart.vue
@@ -2,19 +2,26 @@
 
 <template>
     <div class='cart'>
-        <div v-for='({quantity, product} , key) in items' :key='key'>
-            <CartItem :value='quantity' :name='product.name' :marca='product.marca' :price='product.price'
-            @remove='remove(key)' @increment='increment(key)' @decrement='decrement(key)'
-            @update='x => update(key, x)'>
-                <img :src='require(`@/assets/${product.src}`)' :alt='product.alt'>
-            </CartItem>
-            <hr>
+        <SecondHeader title='Cart'></SecondHeader>
+        <div class="content">
+            <Card class='cartList'>
+                <div v-for='({quantity, product} , key) in items' :key='key'>
+                    <CartItem :value='quantity' :name='product.name' :marca='product.marca' :price='product.price'
+                    @remove='remove(key)' @increment='increment(key)' @decrement='decrement(key)'
+                    @update='x => update(key, x)'>
+                        <img :src='require(`@/assets/${product.src}`)' :alt='product.alt'>
+                    </CartItem>
+                    <hr>
+                </div>
+            </Card>
         </div>
     </div>
 </template>
 
 <script>
+import Card from '@/components/Card'
 import CartItem from '@/components/CartItem'
+import SecondHeader from '@/components/SecondHeader'
 
 const createProduct = (name, price, marca, desc, src, alt) => ({
     name, price, marca, desc, src, alt
@@ -24,7 +31,9 @@ const createProduct = (name, price, marca, desc, src, alt) => ({
 export default {
     name: 'Cart',
     components: {
-        CartItem
+        Card,
+        CartItem,
+        SecondHeader
     },
     data: () => ({
         items: [
@@ -43,19 +52,29 @@ export default {
         ]
     }),
     methods: {
-        remove(item) {
-            this.items.splice(item, 1)
+        remove(index) {
+            this.items.splice(index, 1)
         },
-        increment(item) {
-            this.items[item].quantity++;
+        increment(index) {
+            this.items[index].quantity++
         },
-        decrement(item) {
-            this.items[item].quantity--;
+        decrement(index) {
+            const item = this.items[index]
+            item.quantity--
+            item.quantity = item.quantity < 0 ? 0 : item.quantity
+        },
+        update(index, value) {
+            this.items[index].quantity = value < 0 ? 0 : value
         }
     }
 }
 </script>
 
 <style scoped>
-
+.cartList {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: stretch;
+}
 </style>

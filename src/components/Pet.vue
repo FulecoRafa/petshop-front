8c116f6cc84928ecc9492e2fc55e88a8262450ea
Edws <eduardos.rocha17@gmail.com>
2020-07-22 15:54:37 -0300

    Styled Pet component

--- src/components/Pet.vue
+++ src/components/Pet.vue
@@ -1,14 +1,14 @@
 <template>
     <Card lefty>
-        <div id='profile-content'>
-            <Data id='profile-pic'>
+        <div class='petAvatar'>
+            <Data class='profile-pic'>
                 <img v-if='images[type]' :src='require(`@/assets/${images[type].src}`)' alt="profilePic">
                 <img v-else src='@/assets/biscuit.png' >
             </Data>
-            <Data label='Name'>{{ name }}</Data>
-            <Data label='Type'>{{ capitalize(type) }}</Data>
-            <Data label='Race'>{{ race }}</Data>
-            <Data label='Age'>{{ age }}</Data>
+            <Data class='data' label='Name'>{{ name }}</Data>
+            <Data class='data' label='Type'>{{ capitalize(type) }}</Data>
+            <Data class='data' label='Race'>{{ race }}</Data>
+            <Data class='data' label='Age'>{{ age }}</Data>
             <router-link to='404'>Edit</router-link>
         </div>
     </Card>
@@ -42,5 +42,37 @@ export default {
 </script>
 
 <style scoped>
+.petAvatar{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
 
+.petAvatar *{
+  margin: 10px;
+  padding: 5px;
+}
+
+/* Reducing the space between the label and the data */
+.petAvatar .data ::v-deep h3{
+    margin-right: 0px;
+}
+.petAvatar .data ::v-deep p{
+    margin-left: 0px;
+}
+
+.petAvatar a{
+  background: var(--secondarycolor);
+  width: 100%;
+  border: none;
+  border-radius: 1px;
+  color: white;
+  font-size: 20px;
+  font-weight: 500;
+  cursor: pointer;
+}
+.petAvatar a:hover{
+  filter: brightness(1.2);
+}
 </style>

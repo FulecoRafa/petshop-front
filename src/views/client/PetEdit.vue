662567c20a180cc6672fc6ea579530699a119650
Edws <eduardos.rocha17@gmail.com>
2020-07-23 18:24:07 -0300

    PetEdit

---
+++ src/views/client/PetEdit.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class='petEdit'>
+        <SecondHeader title='Edit Pet'></SecondHeader>
+        <div class='content'>
+            <Card>
+                <PetForm></PetForm>
+            </Card>
+        </div>
+    </div>
+</template>
+
+<script>
+import Card from '@/components/Card'
+import PetForm from '@/components/PetForm'
+import SecondHeader from '@/components/SecondHeader'
+
+export default {
+    name: 'PetEdit',
+    components: {
+        Card,
+        PetForm,
+        SecondHeader
+    }
+}
+</script>
+
+<style>
+
+</style>

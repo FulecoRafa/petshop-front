589e5aed7a6ac46f39ef032232b211a8f6f82150
Edws <eduardos.rocha17@gmail.com>
2020-07-23 18:09:10 -0300

    Added SecondHeader to Pets

--- src/views/client/Pets.vue
+++ src/views/client/Pets.vue
@@ -1,17 +1,22 @@
 <template>
-    <div class='content' style='flex-direction: row;'>
-        <Pet name='Rex' type='dog' race='Shi Tzu' age='4'></Pet>
-        <Pet name='Milicuco' type='cat' race='Show shoes' age='10'></Pet>
+    <div class='pets'>
+        <SecondHeader title='Pets'></SecondHeader>
+        <div class='content' style='flex-direction: row;'>
+            <Pet name='Rex' type='dog' race='Shi Tzu' age='4'></Pet>
+            <Pet name='Milicuco' type='cat' race='Show shoes' age='10'></Pet>
+        </div>
     </div>
 </template>
 
 <script>
 import Pet from '@/components/Pet'
+import SecondHeader from '@/components/SecondHeader'
 
 export default {
     name: 'Pets',
     components: {
-        Pet
+        Pet,
+        SecondHeader
     }
 }
 </script>

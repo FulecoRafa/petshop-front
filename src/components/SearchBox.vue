74919cdd92bfba7226f28f7375e030fc8eb80974
Edws <eduardos.rocha17@gmail.com>
2020-07-24 00:10:10 -0300

    Changing the SearchBox style to similar to forms input

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -2,8 +2,10 @@
     <Card class='searchBox'>
         <h3>Search</h3>
         <form>
+            <span>
             <input type='text' name='searchPrompt' placeholder='Ex.: Dog food' />
             <input type='submit' value='🔎' />
+            </span>
         </form>
     </Card>
 </template>
@@ -27,6 +29,16 @@ export default {
 
 .searchBox input[type=text]{
     width: 70%;
+    background-color: white;
+    font-size: 18px;
+    max-width: 40%;
+    border: none;
+    border-radius: 3px;
+    border-bottom: 5px solid purple;
+    transition: 0.2s ease;
+}
+#profileForm input[type=text]:focus{
+    border-bottom: 5px solid greenyellow;
 }
 
 .searchBox input[type=submit]{

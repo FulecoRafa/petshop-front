55d9136fecd1899e0bf2b8ec6a3b1cbcabf70a76
Edws <eduardos.rocha17@gmail.com>
2020-07-23 23:34:32 -0300

    Created RecCard component and using it

---
+++ src/components/rcard/RecCard.vue
@@ -0,0 +1,61 @@
+<!-- A rectangular card -->
+
+<template>
+    <div class='recCard'>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RecCard'
+}
+</script>
+
+<style>
+.recCard {
+    background: whitesmoke;
+    padding: 20px;
+    width: 240px;
+    box-shadow: 2px 2px 10px rgba(0,0,0,0.3);
+    margin-right: 60px;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+}
+.recCard *{
+    padding: 5px;
+}
+.recCard h5, .recCard a p{
+    color: gray;
+}
+.recCard p{
+    word-break: keep-all;
+}
+.recCard .slot img{
+    height: 150px;
+    object-fit: cover;
+}
+.recCard a {
+    background: var(--secondarycolor);
+    width: 100%;
+    border: none;
+    border-radius: 1px;
+    color: white;
+    font-size: 20px;
+    font-weight: 500;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.recCard a img{
+    height: 24px;
+    padding-right: 10px;
+}
+.recCard a:hover{
+    filter: brightness(1.2);
+}
+</style>
--- src/components/rcard/User.vue
+++ src/components/rcard/User.vue
@@ -1,68 +1,25 @@
 <!-- TODO Translate 'marca' -->
 
 <template>
-    <div class='userCard'>
+    <RecCard>
         <div class='slot'><slot></slot></div>
         <h3>{{ name }}</h3>
         <h4>{{ admin ? 'Admin' : 'Client' }}</h4>
         <router-link to='/admin/useredit'>Edit</router-link>
-    </div>
+    </RecCard>
 </template>
 
 <script>
+import RecCard from '@/components/rcard/RecCard'
+
 export default {
     name: 'User',
     props: [
         'name',
         'admin'
-    ]
+    ],
+    components: {
+        RecCard
+    }
 }
 </script>
-
-<style scoped>
-.userCard {
-    background: whitesmoke;
-    padding: 20px;
-    width: 240px;
-    box-shadow: 2px 2px 10px rgba(0,0,0,0.3);
-    margin-right: 60px;
-    margin-bottom: 30px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-}
-.userCard *{
-    padding: 5px;
-}
-.userCard h5, .userCard a p{
-    color: gray;
-}
-.userCard p{
-    word-break: keep-all;
-}
-.userCard .slot img{
-    height: 150px;
-    object-fit: cover;
-}
-.userCard a{
-    background: var(--secondarycolor);
-    width: 100%;
-    border: none;
-    border-radius: 1px;
-    color: white;
-    font-size: 20px;
-    font-weight: 500;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-.userCard a img{
-    height: 24px;
-    padding-right: 10px;
-}
-.userCard a:hover{
-    filter: brightness(1.2);
-}
-</style>
